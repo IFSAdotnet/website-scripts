05e5bc8b9636564c3f10df719adae440445dc9b3
Simone Massaro (mone27) <massaro.simone.it@gmail.com>
2019-09-12 16:24:20 +0200

    team import using list of users; updated graphics

--- team_page_import.py
+++ team_page_import.py
@@ -4,23 +4,73 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from team_templates import *
+import datetime
 
-# can use name and group description for a better page results
-group_list = ['direction@ifsa.net', 'council@ifsa.net', 'LO@ifsa.net', 'cdc.commission@ifsa.net',
-              'ipc.commission@ifsa.net', 'communication.commission@ifsa.net', 'rrs@ifsa.net']
+users_dir = {'IFSA Board':
+    [
+        'amos.amanubo',
+        'damiano.cilio',
+        'catharina.schmidt',
+        'kirsten.langmaid',
+        'maximilian.schubert',
+        'daniel.guerra',
+        'jiayi.chew'
+    ],
+    'Liason Officers':
+    [
+        'itzhak.lopez',
+        'mahtuf.ikhsan',
+        'matus.urbancik',
+        'volodymyr.kravets',
+        'yulia.cuthbertson'
+    ],
+    'Communication Commission':
+    [
+        'annebel.soer',
+        'simone.massaro',
+        'sylvannisa.putri',
+        'ece.acaroglu',
+        'ezhilan.nambi'
+    ],
+    'International Processes Commission':
+    [
+        'oindrila.basu',
+        'barbara.oellerer',
+        'sara.reimanis',
+        'frederik.buchholz',
+        'alina.lehikoinen',
+        'stipan.cupic'
+    ],
+    'Capacity Development Commission':
+    [
+        'junaid.peters',
+        'fergus.price',
+        'luisa.gragnaniello'
+    ],
+    "Regional Representatives":
+    [
+        'angel.goldsmith',
+        'addison.riddle',
+        'felipe.astorga',
+        'estefani.arroyo',
+        'jilian',
+        'yutung.hung',
+        'mari.tuvikene',
+        'bo.kofod',
+        'elif.duman'
+    ]
+}
 
-# group_list = ['web.commission@ifsa.net']
-
-default_photo = [{'url': 'https://lh4.googleusercontent.com/-MT7weC2IONE/AAAAAAAAAAI/AAAAAAAAAAc/UYN61ez5s-o/s100/photo.jpg'}]
+default_photo = [{
+    'url': 'https://lh4.googleusercontent.com/-MT7weC2IONE/AAAAAAAAAAI/AAAAAAAAAAc/UYN61ez5s-o/s100/photo.jpg'}]
 
 f = open('out.txt', 'w')
 
+# connect to google API ----
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/admin.directory.user',
           'https://www.googleapis.com/auth/admin.directory.group',
           'https://www.googleapis.com/auth/contacts']
-
-
 
 creds = None
 # The file token.pickle stores the user's access and refresh tokens, and is
@@ -45,51 +95,63 @@ service_admin = build('admin', 'directory_v1', credentials=creds)
 service_people = build('people', 'v1', credentials=creds)
 
 
-def print_user(user_mail):
-    user = service_admin.users().get(userKey=user_mail).execute()
+def print_user(user_mail, f):
     print(user_mail)
+    user = service_admin.users().get(userKey=user_mail).execute()
+    photo = service_people.people().get(resourceName="people/" + user['id'], personFields='photos').execute()
     subs = {
         'description': "",
         'job_title': user['organizations'][0]['title'],
         'name': user['name']['fullName'],
         'name_link': mail2id(user['primaryEmail']),
         'email': user['primaryEmail'],
-        # 'pic_url': user.get('thumbnailPhotoUrl', default_photo),
-        'pic_link': "",
-
+        'pic_url': photo.get('photos', default_photo)[0]['url'],
+        'pic_link': ""
     }
-    photo = service_people.people().get(resourceName="people/"+user['id'], personFields='photos').execute()
-    subs['pic_url'] = photo.get('photos', default_photo)[0]['url']
-    #print(photo)
     f.write(str(person.substitute(subs)))
 
 
-def print_group(group_mail):
-    members = service_admin.members().list(groupKey=group_mail).execute()
-    for m in members['members']:
-        if m['type'] == 'USER':
-            print_user(m['email'])
-        elif m['type'] == 'GROUP':
-            print_group(m['email'])
+# def print_group_users(group_mail, f):
+#     members = service_admin.members().list(groupKey=group_mail).execute()
+#     if not 'members' in members:  # guard against empty groups, need to refactor!!
+#         return False
+#     for m in members['members']:
+#         if m['type'] == 'USER':
+#             print_user(m['email'], f)
+#         elif m['type'] == 'GROUP':
+#             print_group_users(m['email'], f)
+
+
+# def print_group_info(group_mail, f):
+#     # get group name
+#     group_title = service_admin.groups().get(groupKey=group_mail).execute()['name']
+#     f.write(str(group.substitute(group_link=group_mail), group_title=group_title)))
+
+def print_group_info(group_name, f):
+    print(group_name)
+    f.write(str(group.substitute(group_link=group_name.replace(' ', '-').lower(), group_title=group_name)))
 
 
 def mail2id(mail):
     return mail[:mail.index('@')]
 
 
+def id2mail(id):
+    return id + "@ifsa.net"
+
+
+def print_import_info(f):
+    f.write(f"<!-- team members imported on {datetime.datetime.now()}-->\n\n")
+
+
 def main():
+    print_import_info(f)
     f.write(str(head.substitute()))
-    for g in group_list:
-        # group name nice way removing ifsa.net suffix and replacing . with spaces
-        f.write(str(group_head.substitute(group_link=mail2id(g), group_title=mail2id(g).replace('.', ' ').capitalize())))
-        print_group(g)
-        f.write(str(group_tail.substitute()))
+    for name, users in users_dir.items():
+        print_group_info(name, f)
+        for user in users:
+            print_user(id2mail(user), f)
     f.close()
-
-
-
-
-
 
 
 if __name__ == '__main__':
--- team_templates.py
+++ team_templates.py
@@ -1,52 +1,42 @@
 from string import Template
-person = Template('''[fusion_builder_column_inner type="1_3" layout="1_3" spacing="" center_content="no" hover_type="none"
- link="" target="_self" min_height="" hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="" background_color="" background_image="" 
-background_position="left top" background_repeat="no-repeat" border_size="0" border_color="" border_style="solid" border_position="all" 
-border_radius="" box_shadow="no" dimension_box_shadow="" box_shadow_blur="0" box_shadow_spread="0" box_shadow_color="" box_shadow_style="" padding_top="" 
-padding_right="" padding_bottom="" padding_left="" 
-dimension_margin="" animation_type="" animation_direction="left" animation_speed="0.3" animation_offset="" last="no"]
+person = Template('''[fusion_builder_column type="1_3" layout="1_3" spacing="50px" center_content="yes" link="" 
+target="_self" min_height="" hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id=""
+background_color="" background_image="" background_image_id="" background_position="left top"
+background_repeat="no-repeat" hover_type="zoomin" border_size="1" border_color="#006633" border_style="solid"
+border_position="all" box_shadow="no" box_shadow_blur="0" box_shadow_spread="0" box_shadow_color=""
+box_shadow_style="" animation_type="" animation_direction="left" animation_speed="0.3" animation_offset="" 
+first="true" last="false" padding_right="15" padding_top="15" padding_bottom="15" padding_left="15"
+border_radius_top_left="7" border_radius_top_right="7" border_radius_bottom_left="7"
+border_radius_bottom_right="7"]
 
-
-[fusion_person name="$name" title="$job_title" picture="$pic_url" picture_id="" pic_link="$pic_link" linktarget="_self" 
-pic_style="" pic_style_blur="" pic_style_color="" pic_bordersize="" pic_bordercolor="" pic_borderradius="" 
-hover_type="none" background_color="" content_alignment="" icon_position="" social_icon_boxed="" 
+[fusion_person name="$name" title="$job_title" picture="$pic_url" picture_id="" pic_link="$pic_link" linktarget="_self"
+pic_style="" pic_style_blur="" pic_style_color="" pic_bordersize="" pic_bordercolor="" pic_borderradius="10px"
+hover_type="none" background_color="" content_alignment="" icon_position="bottom" social_icon_boxed="yes"
 social_icon_boxed_radius="" social_icon_color_type="" social_icon_colors="" social_icon_boxed_colors=""
-social_icon_tooltip="" blogger="" deviantart="" digg="" dribbble="" dropbox="" facebook="" flickr="" forrst="" 
-googleplus="" instagram="" linkedin="" myspace="" paypal="" pinterest="" reddit="" rss=""
-skype="" soundcloud="" spotify="" tumblr="" twitter="" vimeo="" vk="" whatsapp="" xing="" yahoo="" yelp="" youtube="" 
-email="$email" show_custom="no" 
-hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="$name_link"]
+social_icon_tooltip="" blogger="" deviantart="" digg="" dribbble="" dropbox="" facebook="" flickr="" forrst=""
+instagram="" linkedin="" myspace="" paypal="" pinterest="" reddit="" rss="" skype="" soundcloud="" spotify=""
+tumblr="" twitter="" vimeo="" vk="" whatsapp="" xing="" yahoo="" yelp="" youtube="" email="$email" show_custom="yes"
+hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="$name_link" /]
+[/fusion_builder_column]''')
 
-$description
+group = Template('''
 
-[/fusion_person]
-[/fusion_builder_column_inner]''')
-
-group_head = Template('''
-
-[fusion_title hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="$group_link" content_align="left" size="1" font_size="" line_height="" letter_spacing="" margin_top="" margin_bottom="" margin_top_mobile="" margin_bottom_mobile="" text_color="" style_type="default" sep_color=""]
+[fusion_title hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="$group_link"
+ content_align="left" size="1" font_size="" line_height="" letter_spacing="" margin_top="" margin_bottom=""
+  margin_top_mobile="" margin_bottom_mobile="" text_color="" style_type="default" sep_color=""]
 $group_title
 [/fusion_title]
-
-[fusion_builder_row_inner]''')
-group_tail = Template('''[/fusion_builder_row_inner]''')
-
-
-head = Template('''[fusion_builder_container hundred_percent="no" hundred_percent_height="no" hundred_percent_height_scroll="no" hundred_percent_height_center_content="yes" equal_height_columns="no"
- menu_anchor="" hide_on_mobile="small-visibility,medium-visibility,large-visibility" status="published" publish_date="" class="" id="" background_color="" 
-background_image="" background_position="center center" background_repeat="no-repeat" fade="no" background_parallax="none" enable_mobile="no" parallax_speed="0.3" video_mp4="" video_webm="" video_ogv="" video_url="" video_aspect_ratio="16:9" video_loop="yes" video_mute="yes"
- video_preview_image="" border_size="" border_color="" border_style="solid" margin_top="" margin_bottom="" padding_top="" padding_right="" padding_bottom="" padding_left=""]
-[fusion_builder_row]
-[fusion_builder_column type="1_1" layout="1_1" spacing="" center_content="no" link="" target="_self" min_height="" hide_on_mobile="small-visibility,medium-visibility,large-visibility"
- class="" id="" background_color="" background_image="" background_image_id="" 
-background_position="left top" background_repeat="no-repeat" hover_type="none"
- border_size="0" border_color="" border_style="solid" border_position="all" 
-border_radius="" box_shadow="no" dimension_box_shadow="" box_shadow_blur="0"
- box_shadow_spread="0" box_shadow_color="" box_shadow_style="" padding_top=""
- padding_right="" padding_bottom="" padding_left="" margin_top="" margin_bottom="" 
-animation_type="" animation_direction="left" animation_speed="0.3" 
-animation_offset="" last="no"]
 ''')
-tail = Template('''[/fusion_builder_column]
-[/fusion_builder_row]
+
+
+
+head = Template('''[fusion_builder_container hundred_percent="no" equal_height_columns="no" menu_anchor=""
+hide_on_mobile="small-visibility,medium-visibility,large-visibility" class="" id="" background_color=""
+background_image="" background_position="center center" background_repeat="no-repeat" fade="no" 
+background_parallax="none" parallax_speed="0.3" video_mp4="" video_webm="" video_ogv="" video_url=""
+video_aspect_ratio="16:9" video_loop="yes" video_mute="yes" overlay_color="" video_preview_image="" border_size=""
+border_color="" border_style="solid" padding_top="" padding_bottom="" padding_left="" padding_right=""]
+
+''')
+tail = Template('''[/fusion_builder_row]
 [/fusion_builder_container]''')
